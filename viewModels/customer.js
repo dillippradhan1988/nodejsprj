99b3d0498117c2deb56f5febcf5531e78d17a4cb
Dillip <dillippradhan1988@gmail.com>
2016-05-25 03:18:08 +0000

    frm c9 last cmt

---
+++ viewModels/customer.js
@@ -0,0 +1,98 @@
+var Customer = require('../models/customer.js');
+
+// convenience function for joining fields
+function smartJoin(arr, separator){
+	if(!separator) separator = ' ';
+	return arr.filter(function(elt) {
+		return elt!==undefined &&
+			elt!==null &&
+			elt.toString().trim() !== '';
+	}).join(separator);
+}
+
+var _ = require('underscore');
+
+// get a customer view model
+// NOTE: readers of the book will notice that this function differs from the version
+// in the book.  Unfortunately, the version in the book is incorrect (Mongoose does not
+// oofer an asynchronous version of .findById).  My apologies to my readers.
+
+//Method 1
+/*
+function getCustomerViewModel(customer, orders){
+	var vm = _.omit(customer, 'salesNotes');
+	return _.extend(vm, {
+		name: smartJoin([vm.firstName, vm.lastName]),
+		fullAddress: smartJoin([
+			customer.address1,
+			customer.address2,
+			customer.city + ', ' + 
+				customer.state + ' ' + 
+				customer.zip,
+		], '<br>'),
+		orders: orders.map(function(order){
+			return {
+				orderNumber: order.orderNumber,
+				date: order.date,
+				status: order.status,
+				url: '/orders/' + order.orderNumber,
+			};
+		}),
+	});
+}
+module.exports = getCustomerViewModel;
+*/
+
+
+//Method 2
+/*
+module.exports.getCustomerViewModel = function(customer, orders){
+	var vm = _.omit(customer, 'salesNotes');
+	return _.extend(vm, {
+		name: smartJoin([vm.firstName, vm.lastName]),
+		fullAddress: smartJoin([
+			customer.address1,
+			customer.address2,
+			customer.city + ', ' + 
+				customer.state + ' ' + 
+				customer.zip,
+		], '<br>'),
+		orders: orders.map(function(order){
+			return {
+				orderNumber: order.orderNumber,
+				date: order.date,
+				status: order.status,
+				url: '/orders/' + order.orderNumber,
+			};
+		}),
+	});
+}
+*/
+
+
+//Method 3
+module.exports = {
+	getCustomerViewModel : function(customer, orders){
+		console.log(customer);
+		var vm = _.omit(customer, 'salesNotes');
+		console.log(vm);
+		return _.extend(vm, {
+			name: smartJoin([vm.firstName, vm.lastName]),
+			fullAddress: smartJoin([
+				customer.address1,
+				customer.address2,
+				customer.city + ', ' + 
+					customer.state + ' ' + 
+					customer.zip,
+			], '<br>'),
+			orders: orders.map(function(order){
+				return {
+					orderNumber: order.orderNumber,
+					date: order.date,
+					status: order.status,
+					url: '/orders/' + order.orderNumber,
+				};
+			}),
+		});
+	}
+}
